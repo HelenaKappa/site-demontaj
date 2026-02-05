ad4b4d405f6c09904b2e88eb9f1e1d04235bb85f
HelenaKappa <79386354+HelenaKappa@users.noreply.github.com>
2026-02-05 17:48:17 +0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// Инициализация AOS
 AOS.init({
   duration: 800,
   once: true
+});
+
+// Кнопка "Наверх"
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 300) {
+    scrollToTopButton.classList.add('show');
+  } else {
+    scrollToTopButton.classList.remove('show');
+  }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
 });
